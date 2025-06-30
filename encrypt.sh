9a487046d2cd31c33039050b72e67157c22edd7b
Pawel Balawender <pbalawender101@gmail.com>
2025-06-30 13:29:56 +0200

    Try encrypt again

--- encrypt.sh
+++ encrypt.sh
@@ -5,6 +5,10 @@
 # var decrypted = CryptoJS.AES.decrypt(openSSLEncrypted, "Secret Passphrase");
 # Ignore OpenSSL warning "deprecated key derivation used";
 # CryptoJS doesn't seem to support PBKDF2 as key derivation as of June 2025
+# https://github.com/brix/crypto-js/issues/375
+
+# To decrypt in CLI:
+# openssl enc -d -aes-256-cbc -in question.txt.enc -base64
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <filename>"
